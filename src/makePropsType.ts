c81388535219c38433dfb69a4dcc44494c1a3624
c6h4clch3 <c6h4clch3@gmail.com>
2019-05-24 17:31:18 +0900

    initial commit

---
+++ src/makePropsType.ts
@@ -0,0 +1,20 @@
+import { PropOptions } from 'vue/types/options';
+
+type Constructor<T> = new (...args: any[]) => T & object;
+type PropsType<T> = Constructor<T> | (() => T);
+type SomeOf<T> = T | T[];
+type isPrimitive = string | number | boolean | null | undefined | Symbol;
+
+export const makePropsType = <T = any>(
+  type?: SomeOf<PropsType<T>>,
+  required?: boolean,
+  defaultProp?: T extends isPrimitive ? T : () => T,
+  validator?: (value: unknown) => value is T
+): PropOptions<T> => {
+  return {
+    type,
+    required,
+    default: defaultProp,
+    validator
+  };
+};
